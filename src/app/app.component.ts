637918e3cd7583ff4a8a2536a1651ea58edda3b8
kumarandena <kumarandena@gmail.com>
2021-04-10 20:04:39 +0530

    app comp & template update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
-
+import { NavigationEnd, Router, RouterEvent, Event } from '@angular/router';
+import { filter } from 'rxjs/operators';
+interface navItems {
+  displayName: string,
+  route: string,
+  isActive: boolean
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'shoppingcart-ng-apr21';
+
+  navItems: navItems[] = [
+    {
+      displayName: 'Home',
+      route: 'home',
+      isActive: true
+    },
+    {
+      displayName: 'Cart',
+      route: 'cart',
+      isActive: false
+    }
+  ];
+
+  constructor(
+    private router: Router
+  ) {
+    this.router.events.pipe(
+      filter((e: Event): e is RouterEvent => e instanceof NavigationEnd)
+    ).subscribe((e: any) => {
+      if (e['urlAfterRedirects']) {
+        this.navItems.forEach((i) => {
+          if (i.route === e['urlAfterRedirects']) i.isActive = true;
+          else i.isActive = false;
+        })
+      }
+    });
+  }
 }
