f5c246db12a0b8d8a6d30422f567e7e87b7ff8d0
kumarandena <kumarandena@gmail.com>
2021-04-10 21:03:03 +0530

    cart component template and code behind added

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef } from '@angular/core';
 
+interface products {
+  name: string,
+  img: string,
+  metric: string,
+  quantity: number,
+  marketPrice: number,
+  lastSalePrice: number,
+  currentSalePrice: number,
+  count: number
+}
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
@@ -7,9 +18,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
-  constructor() { }
+  products: products[] = [];
+  defaultProducts: products[] = [
+    {
+      name: 'Onion',
+      img: 'https://picsum.photos/200',
+      metric: 'KG',
+      quantity: 1,
+      marketPrice: 90,
+      lastSalePrice: 50,
+      currentSalePrice: 45,
+      count: 1
+    }
+  ];
+
+  constructor(
+    private cdr: ChangeDetectorRef
+  ) { }
 
   ngOnInit(): void {
+    for (let index = 0; index < 5; index++) {
+      this.products[index] = { ...this.defaultProducts[0] }
+    }
+  }
+
+  incrementCount(product: products) {
+    return product.count++;
+  }
+
+  decrementCount(product: products) {
+    return product.count--;
+  }
+
+  get getGrandTotal() {
+    return this.products.map(i => i.currentSalePrice * i.count).reduce((acc, cur) => acc + cur);
+  }
+
+  ngAfterViewChecked(): void {
+    // https://github.com/angular/angular/issues/36173#issuecomment-681282309 
+    this.cdr.detectChanges();
   }
 
 }
