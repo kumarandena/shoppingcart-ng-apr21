2cccb9aaf220321c2ed4aa3a3f18f40f6eed1ea4
kumarandena <kumarandena@gmail.com>
2021-04-10 20:26:30 +0530

    home template base and codebehind

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+interface products {
+  name: string,
+  img: string,
+  metric: string,
+  quantity: number,
+  marketPrice: number,
+  lastSalePrice: number,
+  currentSalePrice: number
+}
+
+interface weightType {
+ weight?: string,
+ count?: string
+}
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +21,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  products: products[] = [];
+  defaultProducts: products[] = [
+    {
+      name: 'Onion',
+      img: 'https://picsum.photos/200',
+      metric: 'KG',
+      quantity: 1,
+      marketPrice: 90,
+      lastSalePrice: 50,
+      currentSalePrice: 45
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
+    for (let index = 0; index < 20; index++) {
+      this.products[index] = this.defaultProducts[0];
+    }
   }
 
 }
