bf83a400116f5093eed75e0a90ef97d2d0357824
kumarandena <kumarandena@gmail.com>
2021-04-11 09:58:36 +0530

    enum added

---
+++ src/app/core/products.ts
@@ -0,0 +1,11 @@
+export interface Products {
+  name: string,
+  img: string,
+  metric: string,
+  quantity: number,
+  marketPrice: number,
+  lastSalePrice: number,
+  currentSalePrice: number,
+  addToCart: boolean,
+  count: number
+}
